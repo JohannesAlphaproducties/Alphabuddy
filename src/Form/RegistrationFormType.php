573574c2e86844b1534799865320b89de9cbbe28
johannesvlot <57399917+johannesvlot@users.noreply.github.com>
2020-04-24 12:51:43 +0200

    je kan een nieuwe gebruiker een rol geven
    yes

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -48,6 +49,13 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
                 'attr' => ['class' => 'form-control']
+            ])
+            ->add('roles', ChoiceType::class , [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'multiple' => true,
+                'choices' => ['Admin' => 'ROLE_ADMIN', 'User' => 'ROLE_USER'],
             ])
         ;
     }
